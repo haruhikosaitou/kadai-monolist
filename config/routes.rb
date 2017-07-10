85b34a37a8cad224fed9c91922a8a75fccfbf18c
haruhikosaitou <harubaggio.00@gmail.com>
2017-07-10 14:19:55 +0000

    ranking

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'rankings/want'
+
   root to: 'toppages#index'
   
   get 'login', to: 'sessions#new'
@@ -10,5 +12,7 @@ Rails.application.routes.draw do
   
   resources :items, only: [:show, :new]
   resources :ownerships, only: [:create, :destroy]
+  
+  get 'rankings/want', to: 'rankings#want'
 end
 
